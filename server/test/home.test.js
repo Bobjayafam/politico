adc2b053b87ae6bce39b0a39686aeb5e8d6de6b8
Bobjayafam <afamjudeo@gmail.com>
2019-01-27 05:14:55 +0100

    chore(server): setup server
    - install eslint as linting library
    - install babel for transpiling ES6 to ES5
    - create project folder structure
    - install express and dotenv
    - configure server.js
    - install mocha and chai for tests
    - write end point for the home route
    - test th endpoint for the home route
    
    [Fnishes #163499719]

---
+++ server/test/home.test.js
@@ -0,0 +1,25 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../server';
+
+chai.use(chaiHttp);
+
+const should = chai.should();
+
+describe('API home route', () => {
+  describe('GET /api/v1', () => {
+    it('should return json', (done) => {
+      chai
+        .request(server)
+        .get('/api/v1')
+        .end((error, res) => {
+          should.not.exist(error);
+          res.status.should.eql(200);
+          res.type.should.eql('application/json');
+          res.body.status.should.equal(200);
+          res.body.data[0].message.should.eql('Welcome to politico API version 1');
+          done();
+        });
+    });
+  });
+});
