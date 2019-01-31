86356e53c2ad8f6091e831ca087c4e44a54a29b4
Bobjayafam <afamjudeo@gmail.com>
2019-02-01 00:59:41 +0100

    chore(server): setup database
    - seed admin data
    - update README
    - add credentials to .env file
    
    [Finishes #ch-setup-database-163623420]

--- server/helpers/Helpers.js
+++ server/helpers/Helpers.js
@@ -1,6 +1,16 @@
+import bcrypt from 'bcrypt';
+
 class Helpers {
   static isEmpty(data) {
     return data.toLowerCase().trim() === '';
+  }
+
+  static hashPassword(password) {
+    return bcrypt.hashSync(password, 10);
+  }
+
+  static comparePassword(password, hash) {
+    return bcrypt.compareSync(password, hash);
   }
 }
 
