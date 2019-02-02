1abe87a3a5dcee49f96822b717afe3f6e392e5a0
Bobjayafam <afamjudeo@gmail.com>
2019-02-02 12:49:43 +0100

    feat(server): implement endpoint for getting all offices
    - change logic of get all officesto ensure data is from the database
    
    {Finishes #163670154]

--- server/controllers/OfficeController.js
+++ server/controllers/OfficeController.js
@@ -35,11 +35,29 @@ class OfficeController {
     }
   }
 
-  static getAllOffices(req, res, next) {
-    res.status(200).json({
-      status: 200,
-      data: [...offices],
-    });
+  static async getAllOffices(req, res, next) {
+    const client = await pool.connect();
+    const query = 'SELECT * FROM offices';
+
+    try {
+      const result = await client.query(query);
+      const { rowCount, rows } = result;
+      if (rowCount <= 0) {
+        res.status(200).json({
+          status: 200,
+          data: [],
+        });
+        return;
+      }
+      res.status(200).json({
+        status: 200,
+        data: rows,
+      });
+    } catch (error) {
+      return res.status(500).json({ status: 500, error: error.message });
+    } finally {
+      await client.release();
+    }
   }
 
   static async getOneOffice(req, res, next) {
