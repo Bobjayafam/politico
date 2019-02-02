81ee09713a9e676576924fd87b560e0798075f75
Bobjayafam <afamjudeo@gmail.com>
2019-02-02 12:17:30 +0100

    feat(server): implement an endpoint to get a specific office
    - change logic to allow using database instead of data structures
    
    [Finished #163670159]

--- server/test/office.test.js
+++ server/test/office.test.js
@@ -6,45 +6,6 @@ chai.use(chaiHttp);
 
 const should = chai.should();
 
-describe('GET /api/v1/offices', () => {
-  it('should return all offices', (done) => {
-    chai.request(server).get('/api/v1/offices')
-      .end((err, res) => {
-        res.status.should.eql(200);
-        done();
-      });
-  });
-
-  it('should return a single office', (done) => {
-    chai.request(server).get('/api/v1/offices/1')
-      .end((err, res) => {
-        should.not.exist(err);
-        res.body.data[0].name.should.eql('president');
-        res.status.should.eql(200);
-        done();
-      });
-  });
-
-
-  it('should not return an office if the id is not valid', (done) => {
-    chai.request(server).get('/api/v1/offices/mnbvn')
-      .end((err, res) => {
-        res.status.should.eql(400);
-        res.body.error.should.eql('Invalid id');
-        done();
-      });
-  });
-
-  it('should return an error if there is no office with the id', (done) => {
-    chai.request(server).get('/api/v1/offices/9')
-      .end((err, res) => {
-        res.status.should.eql(404);
-        res.body.error.should.eql('No office with such id');
-        done();
-      });
-  });
-});
-
 describe('POST /api/v1/offices', () => {
   it('should create a new office', (done) => {
     chai.request(server).post('/api/v1/offices').send({
@@ -93,3 +54,43 @@ describe('POST /api/v1/offices', () => {
       });
   });
 });
+
+describe('GET /api/v1/offices', () => {
+  it('should return all offices', (done) => {
+    chai.request(server).get('/api/v1/offices')
+      .end((err, res) => {
+        res.status.should.eql(200);
+        done();
+      });
+  });
+
+  it('should return a single office', (done) => {
+    chai.request(server).get('/api/v1/offices/1')
+      .end((err, res) => {
+        should.not.exist(err);
+        res.status.should.eql(200);
+        done();
+      });
+  });
+
+
+  it('should not return an office if the id is not valid', (done) => {
+    chai.request(server).get('/api/v1/offices/mnbvn')
+      .end((err, res) => {
+        res.status.should.eql(400);
+        res.body.error.should.eql('Invalid id');
+        done();
+      });
+  });
+
+  it('should return an error if there is no office with the id', (done) => {
+    chai.request(server).get('/api/v1/offices/1000')
+      .end((err, res) => {
+        res.status.should.eql(404);
+        res.body.error.should.eql('No office with such id');
+        done();
+      });
+  });
+});
+
+
