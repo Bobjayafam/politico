c075c5bd27408b97ef5d5a8d77ceeb7d86f1091d
Bobjayafam <afamjudeo@gmail.com>
2019-02-04 01:19:47 +0100

    feat(server): implementation of an endpoint for fetching election result
    - add a get result method to the office controller
    - specify a routes for this endpoint
    
    [Delivers #163683866]

--- server/routes/officesRoute.js
+++ server/routes/officesRoute.js
@@ -8,5 +8,6 @@ router.post('/', Validate.validateOffice, OfficeController.createoffice);
 router.get('/', OfficeController.getAllOffices);
 router.get('/:id', Validate.validateId, OfficeController.getOneOffice);
 router.post('/:id/register', Validate.validateId, OfficeController.registerCandidate);
+router.get('/:id/result', Validate.validateId, OfficeController.getResult);
 
 export default router;
